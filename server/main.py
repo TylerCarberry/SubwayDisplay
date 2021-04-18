d88a5c774071a4c5790ac1437ca1f2aede369fd4
Tyler Carberry <TylerCarberry@users.noreply.github.com>
2021-04-17 23:43:03 -0400

    Disable flask debug mode

--- server/main.py
+++ server/main.py
@@ -91,4 +91,4 @@ def make_image_for_timestamp(timestamp):
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
+    app.run(debug=False, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
